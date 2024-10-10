4354bf4e221064c7d19b1851c710091c0a8f18a4
Bouncyyyy <3361257890@qq.com>
2024-10-10 16:53:16 +0800

    a demo of mutation using libclang, written in cpp, to add variables into code

--- mutate_strategy/add_unused_arrays.cpp
+++ mutate_strategy/add_unused_arrays.cpp
@@ -14,18 +14,62 @@
 using namespace clang;
 using namespace llvm;
 
-class TestASTConsumer : public ASTConsumer {
-  public:
-    TestASTConsumer() {}
+class AddVarASTConsumer;
+AddVarASTConsumer *consumer;
 
-    bool HandleTopLevelDecl(DeclGroupRef D) {
-        std::cout << "handle top level decl" << std::endl;
+class AddVarASTVisitor : public RecursiveASTVisitor<AddVarASTVisitor> {
+  public:
+    AddVarASTVisitor(Rewriter &Prewriter):rewriter(&Prewriter)  {}
+
+    bool VisitVarDecl(VarDecl *vd) {
+        // if global
+        if (vd->hasGlobalStorage()) {
+            std::string vartype = vd->getType().getAsString();
+            std::string varname = vd->getNameAsString();
+
+            // get Loc after semi
+            SourceLocation VDEndLoc = vd->getEndLoc();
+            SourceManager &mgr = rewriter->getSourceMgr();
+            const char *buf = mgr.getCharacterData(VDEndLoc);
+            int offset = 0;
+            while (*buf != ';') {
+                buf++;
+                if(*buf == '\0') break;
+                offset++;
+            }
+            offset++;
+            VDEndLoc = VDEndLoc.getLocWithOffset(offset);
+            rewriter->InsertText(VDEndLoc, "\n" + vartype + "* " + "b=&" + varname+";");
+        }
         return true;
     }
+
+  private:
+    Rewriter *rewriter;
 };
 
-std::unique_ptr<CompilerInstance> getCompilerInstance(std::string infile)
-{
+class AddVarASTConsumer : public ASTConsumer {
+  public:
+    AddVarASTConsumer(Rewriter &rewriter): visitor(rewriter), mgr(nullptr) {}
+
+    void setMgr(SourceManager *Pmgr) {
+        assert(!mgr && "mgr already set!");
+        mgr = Pmgr;
+    }
+
+    bool HandleTopLevelDecl(DeclGroupRef DR) override {
+        for (auto Decl : DR) {
+            visitor.TraverseDecl(Decl);  // 遍历所有声明
+        }
+        return true;
+    }
+
+  private:
+    SourceManager *mgr;
+    AddVarASTVisitor visitor;
+};
+
+std::unique_ptr<CompilerInstance> getCompilerInstance(std::string infile) {
     std::unique_ptr<CompilerInstance> CI = std::make_unique<CompilerInstance>();
     assert(CI && "failed to initialize CI!");
 
@@ -49,17 +93,15 @@ std::unique_ptr<CompilerInstance> getCompilerInstance(std::string infile)
     DgClient.BeginSourceFile(CI->getLangOpts(), &CI->getPreprocessor());
     CI->createASTContext();
 
-    CI->setASTConsumer(std::make_unique<TestASTConsumer>());
-
     InputKind IK = FrontendOptions::getInputKindForExtension(
         StringRef(infile).rsplit('.').second);
     assert(CI->InitializeSourceManager(FrontendInputFile(infile, IK)) &&
            "initialize infile failed!");
+
     return CI;
 }
 
-llvm::raw_ostream *getOutStream(std::string outfile)
-{
+llvm::raw_ostream *getOutStream(std::string outfile) {
     if (outfile.empty())
         return &(llvm::outs());
 
@@ -70,10 +112,10 @@ llvm::raw_ostream *getOutStream(std::string outfile)
     return Out;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     assert(argc == 3 && "should take two args!");
     std::string infile = argv[1], outfile = argv[2];
+
     // init compiler instance
     std::unique_ptr<CompilerInstance> CI = getCompilerInstance(infile);
     // get rewriter
@@ -81,7 +123,11 @@ int main(int argc, char **argv)
     clang::SourceManager &src_mger = ctx.getSourceManager();
     clang::Rewriter rewriter;
     rewriter.setSourceMgr(src_mger, ctx.getLangOpts());
-
+    // set consumer
+    assert(!consumer && "consumer already initialized");
+    consumer = new AddVarASTConsumer(rewriter);
+    consumer->setMgr(&CI->getSourceManager());
+    CI->setASTConsumer(std::unique_ptr<AddVarASTConsumer>(consumer));
     // create sema
     CI->createSema(TU_Complete, 0);
     DiagnosticsEngine &Diag = CI->getDiagnostics();
@@ -97,8 +143,7 @@ int main(int argc, char **argv)
     if (RWBuf) {
         std::cout << "file changed" << std::endl;
         *os << std::string(RWBuf->begin(), RWBuf->end());
-    }
-    else {
+    } else {
         std::cout << "file unchanged" << std::endl;
         FileID MainFileID = src_mger.getMainFileID();
         auto MainBuf = src_mger.getBufferOrNone(MainFileID);
