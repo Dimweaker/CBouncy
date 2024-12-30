66f696675dcde9f848c6b5c27c95464851729371
dimweaker <443668214@qq.com>
2024-12-30 21:48:14 +0800

    开发cut功能

--- dev/cutter.py
+++ dev/cutter.py
@@ -1,7 +1,15 @@
 import re
 
 def cut_parts(ori_code: str):
-    declaration: str = re.search("(.+)(?=/\* --- FUNCTIONS --- \*/)", ori_code, re.S).group()
+    declaration: str = re.search("(.+)(?=/\* --- FUNCTIONS --- \*/)", ori_code, re.S).group().replace(
+        "\nstatic long __undefined;\n", "")
+    declaration = "#pragma once\n" + re.sub("static ", "", declaration)
+    variables_definition: str = re.search("/\* --- GLOBAL VARIABLES --- \*/(.+)/\* --- FORWARD DECLARATIONS --- \*/",
+                                          declaration, re.S).group()
+    variables: list[str] = re.findall("(?<=\n)(.+?=?.+?;)", variables_definition)
+    variables_declaration: str = "\n".join([f"extern {var}" for var in variables]) + "\n"
+    declaration = declaration.replace(variables_definition, variables_declaration)
+
     functions_definition: str = re.search("/\* --- FUNCTIONS --- \*/(.+)(?=int main)", ori_code, re.S).group()
     functions: list[str] = re.findall("\n([^\n]+func_\d+?\([^;]*?\)\s?\{.+?\n})", functions_definition, re.S)
     main: str = re.search("int main \(int argc, char\* argv\[]\)\n{.+?\n}", ori_code, re.S).group()
@@ -17,6 +25,7 @@ def cut_parts(ori_code: str):
 
     with open("main.c", "w") as f:
         f.write("#include \"declaration.h\"\n\n")
+        f.write(variables_definition)
         f.write(main)
 
 
