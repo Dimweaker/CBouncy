2f40a081179bbdf7dc228f72dfc19609dd4ac5ca
dimweaker <443668214@qq.com>
2024-12-30 21:05:16 +0800

    在mutant的function optimize内加入-O0，以保证全局的optimization level不会对function内部产生影响

---
+++ dev/cutter.py
@@ -0,0 +1,27 @@
+import re
+
+def cut_parts(ori_code: str):
+    declaration: str = re.search("(.+)(?=/\* --- FUNCTIONS --- \*/)", ori_code, re.S).group()
+    functions_definition: str = re.search("/\* --- FUNCTIONS --- \*/(.+)(?=int main)", ori_code, re.S).group()
+    functions: list[str] = re.findall("\n([^\n]+func_\d+?\([^;]*?\)\s?\{.+?\n})", functions_definition, re.S)
+    main: str = re.search("int main \(int argc, char\* argv\[]\)\n{.+?\n}", ori_code, re.S).group()
+
+    with open("declaration.h", "w") as f:
+        f.write(declaration)
+
+    for func in functions:
+        func_name: str = re.search("func_\d+", func).group()
+        with open(f"{func_name}.c", "w") as f:
+            f.write("#include \"declaration.h\"\n\n")
+            f.write(func)
+
+    with open("main.c", "w") as f:
+        f.write("#include \"declaration.h\"\n\n")
+        f.write(main)
+
+
+
+if __name__ == "__main__":
+    with open("orig.c", "r") as f:
+        ori_code = f.read()
+    cut_parts(ori_code)
